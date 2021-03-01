42f382d2887629d5a711538576c8c29d5427f2b0
Richard Ng <richardkentng@gmail.com>
2021-02-28 22:02:42 -0800

    changed login route to /

--- middleware/isLoggedIn.js
+++ middleware/isLoggedIn.js
@@ -1,7 +1,7 @@
 module.exports = (req, res, next) => {
   if (!req.user) {
     req.flash('error', 'You must be logged in to visit this page.')
-    res.redirect('/auth/login')
+    res.redirect('/')
   } else {
     next()
   }
